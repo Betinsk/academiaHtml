3f95cc6541595a2b7c96dddab0a7ed2a76594105
Betinsk <Gmdust@hotmail.com>
2023-07-11 16:49:59 -0300

    inserindo html no template

---
+++ WorkOutProject/index.js
@@ -0,0 +1,12 @@
+
+var nav = document.querySelector('nav')
+
+var ul = document.createElement('ul')
+var li = document.createElement('li')
+var a = document.createElement('a')
+
+nav.appendChild(ul)
+ul.appendChild(li)
+li.appendChild(a)
+
+
